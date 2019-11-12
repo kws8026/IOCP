fe258cec561754d6417ec8bfd0de41a32884fcb6
KangWooSeouk <kws8026@naver.com>
2019-11-12 18:25:09 +0900

    오브젝트 풀 구조변경
    링크드 리스트에서 배열 구조로 변경

--- IocpLib/Objectpool.h
+++ IocpLib/Objectpool.h
@@ -6,123 +6,98 @@
 #include "Logger.h"
 #include <Windows.h>
 
-template <typename T>
-class cObjectPool : cSingleton<cObjectPool> // 싱글톤 패턴을 사용하여 오브젝트마다 하나의 오브젝트풀만 만들도록 함
+template <typename Object>
+class cObjectPool : cSingleton<cObjectPool>				// 싱글톤 패턴을 사용하여 오브젝트마다 하나의 오브젝트풀만 만들도록 함
 {
-	struct  Node{
-		T* pData;
-		Node* pNext;
-		Node():pData(nullptr),pNext(nullptr)
-		{
-				pData = new T
-		};
-		~Node() { 
-			if (pData != nullptr) {
-			delete pData }
-		};
-	};
-	Node*			front;			// 리스트의 첫 노드
-	Node*			end;			// 리스트의 말단 노드
-	Node*			assigned;		// 할당 리스트의 첫 노드
-	size_t			capacity;		// 리스트의 캐퍼시티
-	size_t			size;			// 할당 리스트의 크기
-	bool			bExpansion;		// 캐퍼시티를 초과할 경우 확장할지를 결정할 플래그
+	Object**		objects;							// 오브젝트의 포인터가 적재된 배열
+	DWORD			capacity;							// 리스트의 캐퍼시티
+	DWORD			size;								// 할당 리스트의 크기
+	bool			bExpansion;							// 캐퍼시티를 초과할 경우 확장할지를 결정할 플래그
 
 	bool	Expansion() {
-		Node* temp = new Node;
-		end->pNext = temp;
-		for (int i = 1; i < capacity; i++) {
-			Node* other = new Node;
-			temp->pNext = other;
-			temp = other;
-			if (other == NULL) {
-				ERROR_CODE("Create Node Failed");
+		DWORD expansion = capacity*2;					// 2배로 확장된 캐퍼시티
+		Object** temp = new Object* [expansion];		// 새로운 오브젝트 배열 생성
+		if (temp == NULL) {			
+			ERROR_CODE("Failed Create New Objects");
+			return false;
+		}
+		ZeroMemory(temp, sizeof(Object*) * expansion);
+		for (int i = 0; i < capacity; i++) {
+			temp[i] = objects[i];						// 기존 오브젝트들의 정보를 복사
+		}
+		for (int i = capacity; i < expansion; i++) {	// 확장된 캐퍼시티만큼 오브젝트를 생성
+			temp[i] = new T;
+			if (temp[i] == NULL) {
+				ERROR_CODE("Failed Create New Object");
+				delete[] temp;
 				return false;
 			}
 		}
-		end = temp;
-		capacity *= 2;
+		delete[] objects;
+		objects = temp;
+		capacity = expansion;
 		return true;
 	}
 public:
-	cObjectPool():front(nullptr), end(nullptr), assigned(nullptr), capacity(0), size(0), bExpansion(false){
+	cObjectPool():Objects(nullptr), capacity(0), size(0), bExpansion(false){
 	};
 	~cObjectPool() {
+		if (capacity == 0)
+			return;
+		for (int i = 0; i < capacity; i++) {
+			delete objects[i];
+			objects[i] = NULL;
+		}
+		delete[] objects;
 	};
-	T*		CreatePool(size_t _capacity, bool expansion = false) {
+	bool	CreatePool(DWORD _capacity, bool expansion = false) {
 		if (capacity != 0){
 			ERROR_CODE("Already Created ObjectPool");
-			return nullptr;	// 이미 캐퍼시티가 지정 되어있으면 실패
+			return nullptr;				// 이미 캐퍼시티가 지정 되어있으면 실패
 		}
 		capacity = _capacity;
 		bExpansion = expansion;
-		front = new Node;
-		Node* temp = front;
-		for (int i = 1; i < capacity; i++) {
-			Node* other = new Node;
-			temp->pNext = other;
-			temp = other;
-			if (other == NULL) {
-				ERROR_CODE("Create Node Failed");
+		objects = new Object*[capacity];
+		if (objects == NULL) {
+			ERROR_CODE("Failed Create New Objects");
+			return nullptr;
+		}
+		ZeroMemory(objects, sizeof(Object*)*capacity);
+		for (int i = 0; i < capacity; i++) {
+			objects[i] = new T;
+			if (objects[i] == NULL) {
 				return false;
 			}
 		}
-		end = temp;
 		LOG("Create ObjectPool objects : %d",capacity);
 		return true;
 	};
-	T*		New() {
-		if (capacity == size) {		// 오브젝트가 최대치만크 생성된 경우
-			if (!bExpansion) {		// 더 이상 오브젝트를 생성할 수 없음
+	Object*	New() {
+		if (capacity == size) {			// 오브젝트가 최대치만크 생성된 경우
+			if (!bExpansion) {			// 더 이상 오브젝트를 생성할 수 없음
 				ERROR_CODE("ObjectPool is fulled");
 				return nullptr 
 			}
-			else if(!Expansion()) {	// 확장에 실패할 경우
+			else if(!Expansion()) {		// 확장에 실패할 경우
 				return nullptr		
 			}
 		}
-		size++;
-		auto pReturn = front;
-		front->pNext = pReturn;		// 사용한 노드를 리스트에서 삭제
-		pReturn->pNext = assigned;	// 사용한 노드를 할당 리스트에 등록
-		assigned = pReturn;
-
-		return pReturn.pData;
+		auto pReturn = objects[size++];
+		ZeroMemory(pReturn,sizeof(Object));
+		return pReturn;
 	};
 	bool	Delete(T* object) {
-		Node* temp;								// 이전 노드를 임시저장함
-		auto iter = assigned;					// 할당 리스트에서 탐색함
-		for (;iter != nullptr; iter = iter.pNext) {
-			if (iter.pData == object) {			// 오브젝트를 가진 노드를 발견함
-				ZeroMemory(object, sizeof(T));	// 오브젝트를 초기화 시킴
-				if (temp != nullptr) {			// 오브젝트를 가진 노드가 할당 리스트의 첫 노드일 경우
-					temp->pNext = iter->pNext;	// 할당 리스트에서 제거함
-				}
-				end->pNext = iter;				// 리스트의 말단에 다시 등록함
-				end = iter;
+		for (int i = 0; i < size; i++) {
+			if (objects[i] = object) {
+				auto target = object[--size];   // 사이즈를 감소시키고 말단의 오브젝트를 임시저장
+				object[size] = objects[i];		// 해제한 오브젝트를 배열의 말단으로 배치
+				objects[i] = target;			// 임시저장된 오브젝트를 해제한 오브젝트가 있던 위치로 이동
+				return true;
 			}
-			temp = iter;
 		}
-		return false;				// 할당 리스트에서 해당하는 오브젝트를 찾지 못했을 경우
+		ERROR_CODE("Invalied Object Pointer");
+		return false;							// 할당 리스트에서 해당하는 오브젝트를 찾지 못했을 경우
 	};
-	void	Destroy() {
-		auto iter = assigned;					// 할당 리스트에서 탐색함
-		for (;iter != nullptr;) {
-			auto temp = iter;
-			iter = iter->pNext;
-			delete(temp->pNext);
-			temp->pNext = nullptr;
-			delete(temp);
-		}
-		auto iter = front;					// 리스트에서 탐색함
-		for (;iter != nullptr;) {
-			auto temp = iter;
-			iter = iter->pNext;
-			delete(temp->pNext);
-			temp->pNext = nullptr;
-			delete(temp);
-		}
-	}
 };
 
 
